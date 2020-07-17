ddd6b659a19df3a968328d68296e44942c456b3a
Spandan Dutta <spandydutta@gmail.com>
2020-07-17 08:42:28 +0530

    Area of a circle

--- src/ScannerCircle.java
+++ src/ScannerCircle.java
@@ -6,8 +6,8 @@ public class ScannerCircle {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the radius");
-        double radius = s.nextDouble();
-        double area = (22 * radius * radius) / 7;
+        double r = s.nextDouble();
+        double area = (22 * r * r) / 7;
         System.out.println("Area of a circle: " + area);
     }
 
